dfe6fafae987f800992a0eb41f9e9378ab5d1238
Spyder-code <luaysyauqillah@gmail.com>
2023-03-31 15:41:05 +0700

    update css

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -20,7 +20,7 @@
         <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
         <link href="{{ asset('assets/css/icons.css') }}" rel="stylesheet" type="text/css" />
         <link href="{{ asset('assets/css/metismenu.min.css') }}" rel="stylesheet" type="text/css" />
-        <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet" type="text/css" />
+        <link href="{{ asset('assets/css/style.css?time=').date('His') }}" rel="stylesheet" type="text/css" />
         <link href="{{ asset('assets/css/bootstrap-chosen.css') }}" rel="stylesheet" type="text/css"/>
         <link href="{{ asset('assets/css/yellow.css')}}" rel="stylesheet" />
         <link href="{{ asset('assets/css/bootstrap-fileinput.css')}}" rel="stylesheet" />
